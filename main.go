42290c108de1223e678b1145d5c01ce37a371ebb
hmdyt <konnyaku108@gmail.com>
2023-03-26 01:47:49 +0900

    add experimental progress bar

--- main.go
+++ main.go
@@ -9,19 +9,30 @@ import (
 
 	"github.com/hmdyt/madago/decoder"
 	"github.com/hmdyt/madago/encoder/root"
+	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
 	flag.Parse()
 	path := flag.Arg(0)
 
+	// open file
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatalf("file open %s: %s", path, err.Error())
 	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		log.Fatalf("file stat %s: %s", path, err.Error())
+	}
 	defer file.Close()
 
-	reader := bufio.NewReader(file)
+	// progress bar
+	bar := progressbar.DefaultBytes(fileInfo.Size(), "Decoding")
+	fileReader := progressbar.NewReader(file, bar)
+
+	// decoder
+	reader := bufio.NewReader(&fileReader)
 	d := decoder.NewDecoder(reader, binary.BigEndian)
 
 	events, err := d.Decode()
